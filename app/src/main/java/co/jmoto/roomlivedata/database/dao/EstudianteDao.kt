d83ca744fd05f30c67ba387af2470ca210ffc64e
Jaime Flautero <flaujaim@colpatria.com>
2020-10-12 21:19:53 -0500

    Creacion de entidad y DAO

---
+++ app/src/main/java/co/jmoto/roomlivedata/database/dao/EstudianteDao.kt
@@ -0,0 +1,24 @@
+package co.jmoto.roomlivedata.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import co.jmoto.roomlivedata.database.model.Estudiante
+
+//DAO: Referencia al patrón Data Access Object
+//Operaciones
+
+@Dao
+interface EstudianteDao{
+
+    @Insert
+    fun insert(estudiante: Estudiante)
+
+    @Update
+    fun update(estudiante: Estudiante)
+
+    //consulta SQL
+    @Query("DELETE FROM ESTUDIANTE")
+    fun deleteAll()
+}
---
+++ app/src/main/java/co/jmoto/roomlivedata/database/model/Estudiante.kt
@@ -0,0 +1,19 @@
+package co.jmoto.roomlivedata.database.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Estudiante(
+    @PrimaryKey
+    var id: Long = 0L,
+    var nombres: String,
+    @ColumnInfo(name = "primer_apellido")
+    var primerApellido: String,
+    @ColumnInfo(name = "segundo_apellido")
+    var segundoApellido: String,
+    var asistencia: Int,
+    @ColumnInfo(name = "premio")
+    var ganadoPremio: Boolean
+)
